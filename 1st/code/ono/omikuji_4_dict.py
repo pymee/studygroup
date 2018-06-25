56852eabbe9351c3952b7dcb57702a4331d225d7
jin kanai <inumayugeh@gmail.com>
2018-06-25 09:47:19 +0900

    小野さん変更分追加

--- 1st/code/ono/omikuji_4_dict.py
+++ 1st/code/ono/omikuji_4_dict.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # coding: utf-8
 
 import random
@@ -6,14 +7,12 @@ omikuji = {'大吉': 'すべてよし',
            '中吉': 'まあまあよし',
            '吉': 'よし',
            '凶': 'わるし'}
-
+           
 # 型の表示
 print(type(omikuji))
 
 # 項目の追加
 omikuji['大凶'] = 'すべてわるし'
-
-#printを挟んで、確認してもらうのもあり
 
 # 項目の変更
 omikuji['中吉'] = 'まぁまぁよし'
@@ -24,8 +23,11 @@ del omikuji['凶']
 # omikujiの項目を表示
 print(omikuji)
 
-# リストに変換し、ランダムに表示
-print(random.choice(list(omikuji.items())))
+# 辞書のアイテムをリストに変換
+output_omikuji = list(omikuji.items())
+
+# output_omikujiを表示
+print(random.choice(output_omikuji))
 
 # 型の表示
-print(type(list(omikuji)))
+print(type(output_omikuji))
