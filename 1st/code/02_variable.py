3582820429e75b6ae9f0954636b53b6e7adbad78
juke211 <juke21roa@gmail.com>
2018-07-09 02:20:09 +0900

    omikuji_1 >> daikichi

--- 1st/code/02_variable.py
+++ 1st/code/02_variable.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# 変数(omikuji_1 ~ omikuji_5)に値(運勢)を代入
-omikuji_1 = '大吉 すべてよし'
-omikuji_2 = '中吉 まあまあよし'
-omikuji_3 = '小吉 よし'
-omikuji_4 = '吉 すこしよし'
-omikuji_5 = '凶 わるし'
+# 変数に値(運勢)を代入
+daikichi = '大吉 すべてよし'
+chukichi = '中吉 まあまあよし'
+shokichi = '小吉 よし'
+kichi = '吉 すこしよし'
+kyo = '凶 わるし'
 
-# unseiにomikuji_1を代入
-unsei = omikuji_1
+# unseiにdaikichiを代入
+unsei = daikichi
 
 print('あなたの名前を入力してください')
 
--- 1st/code/03_ransu_if.py
+++ 1st/code/03_ransu_if.py
@@ -5,11 +5,11 @@
 import random
 
 # 代入
-omikuji_1 = '大吉 すべてよし'
-omikuji_2 = '中吉 まあまあよし'
-omikuji_3 = '小吉 よし'
-omikuji_4 = '吉 すこしよし'
-omikuji_5 = '凶 わるし'
+daikichi = '大吉 すべてよし'
+chukichi = '中吉 まあまあよし'
+shokichi = '小吉 よし'
+kichi = '吉 すこしよし'
+kyo = '凶 わるし'
 
 # random.randrangeは()にしていた範囲からランダムに要素を返します
 # 選択した数字をnumに代入
@@ -17,15 +17,15 @@ num = random.randrange(0,5)
 
 # numに格納された数字によって出力される文字列を変更
 if num == 0:
-	unsei = omikuji_1
+	unsei = daikichi
 elif num == 1:
-	unsei = omikuji_2
+	unsei = chukichi
 elif num == 2:
-	unsei = omikuji_3
+	unsei = shokichi
 elif num == 3:
-	unsei = omikuji_4
+	unsei = kichi
 elif num == 4:
-	unsei = omikuji_5
+	unsei = kyo
 else:
 	unsei = ""
 
