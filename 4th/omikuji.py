52d5a930357d3a931a4197fcf2ff8b1d7ac81943
root <itoh881@yahoo.co.jp>
2019-01-21 20:33:55 +0900

    2回目のレビューを反映2回目のレビューを反映

--- 4th/omikuji.py
+++ 4th/omikuji.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.6
 import sys
 import random
 import os
@@ -13,17 +14,7 @@ try:
     with open(sys.argv[1], "r",encoding="utf-8") as f:
 
 #while実行前に定義、この時点でファイルの1行目を読み込み
-        line = f.readline()
-
-#whileで読み込んだ行を格納する空のリストを準備
-        lines=[]
-
-#引数ファイルを１行ずつ読み込んでリストに格納
-        while line:
-            lines.append(line)
-            line = f.readline()
-            if line == "":
-                break
+        lines = f.readlines()
 
 #例外をキャッチした場合の処理内容
 except FileNotFoundError:
@@ -33,34 +24,38 @@ except PermissionError:
         sys.stderr.write("エラー:指定されたファイルに読み取り権限がありません。\n")
         sys.exit(1)
 
-#おみくじの結果を格納したリストからランダムで１行をチョイス
-kekka = random.choice(lines)
+#末尾業を取り除きおみくじ結果の2次元配列を作成
+KekkaList = []
+i = 0
+while lines[i] != "EOF\n":
+    kekka = lines[i]
+    KekkaList.append(kekka.split(','))
+    i = i+1
 
-#ランダムでチョイスした１行（文字列）をカンマ区切りでリストに変換
-kekkaList = kekka.split(',')
+#ランダムで運勢を選択
+unsei = random.choice(KekkaList)
 
 #出力するファイル名に付与する日付を取得
 hizuke = datetime.now().strftime("%Y-%m-%d")
 
 #出力ファイル名を指定
-kekka_filename = r"./results/kekka_{0}.txt".format(hizuke)
+unsei_filename = r"./results/unsei_{0}.txt".format(hizuke)
 
 #出力ファイル名でファイルオープンし結果を書き込み
 try:
-    f = open(kekka_filename,'x')
+    f = open(unsei_filename,'x')
+
+#例外をキャッチした場合の処理内容
 except FileExistsError:
         sys.stderr.write("エラー:同じ名前のファイルが既に存在しています。\n")
         sys.exit(1)
 except FileNotFoundError:
         sys.stderr.write("エラー:ディレクトリが見つかりません。\n")
         sys.exit(1)
-except PermissionError:
-        sys.stderr.write("エラー:指定されたファイルに書き込み権限がありません。\n")
 
-#出力ファイル名でファイルオープンし結果を書き込み
-f = open(kekka_filename,'w')
+#おみくじの結果を書き込み
 f.write("===============================\n")
-for i in kekkaList:
+for i in unsei:
     f.write(i)
     f.write("\n")
 f.write(hizuke)
@@ -68,4 +63,4 @@ f.write("\n")
 f.write("===============================\n")
 f.close()
 
-print("おみくじの結果を{0}に書込みました！".format(kekka_filename))
+print("おみくじの結果を{0}に書込みました！".format(unsei_filename))
